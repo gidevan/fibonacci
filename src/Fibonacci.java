da84593dbfd012f744e814d1f786b75769834f41
gidevan <vsanyc@gmail.com>
2016-12-04 02:09:54 +0300

    fibonacci

---
+++ src/Fibonacci.java
@@ -0,0 +1,25 @@
+/**
+ * Created by vano on 4.12.16.
+ */
+public class Fibonacci {
+
+    private static final int COUNT = 12;
+
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci();
+        for(int i = 0; i < COUNT; i++ ) {
+            System.out.println("fibonacci(" + i + ")=" + fibonacci.fibonacci(i));
+        }
+
+    }
+
+    private int fibonacci(int i) {
+        if(i == 0) {
+            return 0;
+        } else if (i == 1) {
+            return 1;
+        } else {
+            return fibonacci(i - 1) + fibonacci(i - 2);
+        }
+    }
+}
